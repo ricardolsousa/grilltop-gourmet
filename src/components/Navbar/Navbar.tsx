110267756c6b796d127258dd2105fd4ad1985bb9
Ricardo Lopes Sousa <2170885@my.ipleiria.pt>
2023-12-21 20:06:45 +0000

    feat(ui): added images/styles/links on navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,20 @@
-
+import "../Navbar/Navbar.css";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
 
     return (
-        <div>
-            Navbar
+        <div className="navbar">
+            <div className="navbar-logo" >
+                <Link to="/">
+                    <img src="/images/grilltop-gourmet.svg" alt="" className="navbar-logo-svg" />
+                </Link>
+            </div>
+            <div className="navbar-cart">
+                <Link to="/payment">
+                    <img src="/images/cart.svg" alt="" className="navbar-cart-svg" />
+                </Link>
+            </div>
         </div>
     )
 }
