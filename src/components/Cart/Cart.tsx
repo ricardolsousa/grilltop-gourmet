afd4aab19467c2e07a500f844c8e9b971ca315a8
Ricardo Lopes Sousa <2170885@my.ipleiria.pt>
2023-12-28 19:54:04 +0000

    feat(ui): add cart modal and update data on navbar

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,12 +1,64 @@
 
-const Cart = () => {
+import { useEffect, useRef } from "react";
+import "../Cart/Cart.css";
+import { connect } from "react-redux";
+import CartTable from "./Table/CartTable";
+import CartEmpty from "./Empty/CartEmpty";
+
+interface CartProps {
+    cart: {
+        name: string;
+        description: string;
+        price: number;
+        quantity: number;
+    }[];
+    setShowModal: (show: boolean) => void;
+}
+
+const Cart = (props: CartProps) => {
+
+    const { cart, setShowModal } = props;
+    const cartModalRef = useRef<HTMLDivElement>(null);
+    const closeModalRef = useRef<HTMLDivElement>(null);
+
+    const closeModal = (event: MouseEvent) => {
+        if (cartModalRef.current && (!cartModalRef.current.contains(event.target as Node))) {
+            setShowModal(false);
+        }
+    };
+
+    useEffect(() => {
+        document.addEventListener('mousedown', closeModal);
+
+        return () => {
+            document.removeEventListener('mousedown', closeModal);
+        };
+    }, []);
 
     return (
-        <div>
-            Cart
+        <div className="cart-modal">
+            <div className="cart-modal-content" ref={cartModalRef}>
+                <div className="cart-modal-close" onClick={() => setShowModal(false)}>
+                    <span>X</span>
+                </div>
+                <div className="cart-modal-title">
+                    Cart
+                </div>
+                {cart.length ?
+                    <CartTable />
+                    :
+                    <CartEmpty />
+                }
+            </div>
         </div>
     )
 
 }
 
-export default Cart;
+const mapStateToProps = (state: any) => {
+    return {
+        cart: state.cart.cart,
+    };
+};
+
+export default connect(mapStateToProps)(Cart);
